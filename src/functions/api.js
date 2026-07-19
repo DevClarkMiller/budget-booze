53dcc426ae7d6b8d2e5c11a6b7a58b2133af9024
Clark Miller <155762261+DevClarkMiller@users.noreply.github.com>
2026-07-19 18:23:16 +0200

    Changed domain (#9)
    * updated packages, also changed references to old domain to be for the new one
    
    * Fixed remaining refs to old domain

--- src/functions/api.js
+++ src/functions/api.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export default axios.create({
   baseURL: process.env.REACT_APP_API_BASE
     ? process.env.REACT_APP_API_BASE
-    : "https://budgetbooze.ca/api",
+    : "https://budgetbooze.clarkmiller.ca/api",
 });
